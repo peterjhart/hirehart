3a4c76e122d565af076a080fc5ecdb899c978f73
Peter J. Hart <peter@peterjhart.com>
2024-07-31 18:06:47 -0700

    Create a production-only index file and add Google Analytics to it.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import * as fs from 'node:fs'
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'index.production.html',
+    },
+  },
+  plugins: [react(), renameIndexPlugin()],
   server: {
     host: '0.0.0.0',
     port: 3000,
   },
 })
+
+function renameIndexPlugin() {
+  return {
+    name: 'rename-index',
+    closeBundle() {
+      fs.renameSync('dist/index.production.html', 'dist/index.html')
+    },
+  }
+}
